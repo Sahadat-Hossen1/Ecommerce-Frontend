71fc9e46f2892a919051b33f91fa74128346a5ec
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2025-11-08 19:03:49 +0600

    basic setup is done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    < >
+    <div className='flex items-center'>
+      <NavLink></NavLink>
+  <h1 className='text-center text-2xl text-red-500 mx-auto '>Hello World</h1>
+    </div>
+    </>
+  )
+}
