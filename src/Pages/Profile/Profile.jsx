268200580dadd41beb80a0a47558a07cca83679c
Sahadat-Hossen1 <sahadat.hossen1435@gmail.com>
2025-12-19 00:42:40 +0600

    setup .env file for firebase config security

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -5,11 +5,15 @@ import { useNavigate } from "react-router-dom";
 export default function Profile() {
   const { User,handleSignOut } = use_Auth_Data_Context();
     const navigate = useNavigate();
-
+    // const x=import.meta.env.VITE_NAME
+    // console.log(x);
+    
+    
   const SignOut=()=>{
 handleSignOut().catch(error=>console.log(error))
 navigate('/')
   }
+
   return (
     <div>
       <h1>Profile user name:{User?.displayName} </h1>
